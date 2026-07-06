9ba23491d4c3353d27d2d1cc3bf689e3e26ee3d9
Matt <matthewpaulh@hotmail.co.uk>
2026-07-06 19:31:06 +0000

    Added user roles

--- django_api/herd/models/farm.py
+++ django_api/herd/models/farm.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 
 
 class Farm(models.Model):
@@ -7,6 +8,31 @@ class Farm(models.Model):
     address_line_1 = models.CharField(max_length=200)
     postcode = models.CharField(max_length=10)
     country = models.CharField(max_length=100)
+    users = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="farms",
+        through="FarmMembership",
+    )
+
+
+class Membership(models.TextChoices):
+    ADMIN = "admin", "Admin"
+    MEMBER = "member", "Member"
+
+
+class FarmMembership(models.Model):
+    farm = models.ForeignKey(Farm, on_delete=models.CASCADE, related_name="memberships")
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="memberships")
+
+    role = models.CharField(max_length=50, default="member", choices=Membership.choices)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(
+                fields=["farm", "user"],
+                name="unique_farm_membership",
+            )
+        ]
 
 
 class Pasture(models.Model):
